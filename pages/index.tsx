65a4c2fe595743e9be69ebb4c54c6a9372aba3a5
sand6009 <98716952+sand6009@users.noreply.github.com>
2025-05-19 16:07:07 -0400

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Welcome to AncientDoc.com</h1>
+      <p>This site will train you to evaluate the authenticity of Roman to medieval manuscripts.</p>
+    </div>
+  );
+}
